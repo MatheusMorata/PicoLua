f760114a685e5a8bee65f20b35ea2e81f10e086a
MatheusMorata <matlucar09@hotmail.com>
2025-12-01 20:55:42 -0300

    feat: Novos módulos iniciados

--- src/pico.lua
+++ src/pico.lua
@@ -2,25 +2,44 @@ local SDL = require("SDL")
 
 local pico = {}
 
-function pico.iniciar(on) 
-    if on then 
-        SDL.init{SDL.flags.Video} 
-    
+local SDL  = require("SDL")
+local TTF  = require("SDL.ttf")
+local Mix  = require("SDL.mixer")
+
+local pico = {}
+
+
+function pico.iniciar(on)
+    if on then
+        SDL.init{ SDL.flags.Video }
+
         janela = SDL.createWindow{
-            title = 'Pico Lua', 
-            width = 800, 
+            title  = "Pico Lua",
+            width  = 800,
             height = 600,
-            flags = {SDL.window.Resizable}, 
-            x = SDL.window.centralized, 
-            y = SDL.window.centralized, 
+            x      = SDL.window.centralized,
+            y      = SDL.window.centralized,
+            flags  = { SDL.window.Shown }
         }
 
         renderizador = SDL.createRenderer(janela, -1, SDL.rendererFlags.Accelerated)
 
-    else 
+        TTF.init()
+        --Mix.openAudio(22050, Mix.audioFormat.S16SYS, 2, 1024)
+
+    else
+        if pico.font and pico.font.ttf then
+            pico.font.ttf:close()
+        end
+
+        Mix.closeAudio()
+        TTF.quit()
+
+        if renderizador then renderizador:destroy() end
+        if janela then janela:destroy() end
+
         SDL.quit()
     end
-    
 end
 
 return pico
