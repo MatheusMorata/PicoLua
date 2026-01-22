8a278ba6556b59162b808bbfedc185d6db6f83b6
MatheusMorata <matlucar09@hotmail.com>
2026-01-22 16:25:58 -0300

    test: comentário

--- tst/main.lua
+++ tst/main.lua
@@ -2,6 +2,6 @@ local pico = dofile("../src/pico.lua")
 local titulo = "Novo"
 
 
-pico.init(true)
+pico.init(true) -- Inicializa
 pico.set_title(titulo) -- Define o título da janela
 pico.input_delay(2000) -- Mantém a janela aberta no tempo de 2000 milisegundos
