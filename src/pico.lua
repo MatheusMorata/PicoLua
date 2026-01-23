314708cb71168d2d4d2df311456b01c2c695e72b
MatheusMorata <matlucar09@hotmail.com>
2026-01-23 14:11:36 -0300

    feat: Movendo config para pico.lua

--- src/pico.lua
+++ src/pico.lua
@@ -1,60 +1,77 @@
 local pico = {}
 
 local SDL   = require "SDL"
-local TTF   = require "SDL.ttf" 
+local TTF   = require "SDL.ttf"
 local MIXER = require "SDL.mixer"
+
+local function Pico_Dim(w, h)
+    return { w = w or 0, h = h or 0 }
+end
+
+local function PICO_DIM_PHY()
+    return Pico_Dim(500, 500)
+end
+
+local function PICO_DIM_LOG()
+    return Pico_Dim(100, 100)
+end
+
+local function PICO_SIZE_KEEP()
+    return Pico_Dim(0, 0)
+end
+
+local function PICO_SIZE_FULLSCREEN()
+    return Pico_Dim(0, 1)
+end
+
+local function PHY(window)
+    local w, h = window:getSize()
+    return Pico_Dim(w, h)
+end
+
 local TEX = nil
-local CONFIG = dofile("../src/config.lua")
-local PICO_CLIP_RESET = {0, 0, 0, 0}
+local title = "Titulo"
+local undefined = "0x1FFF0000"
 local DEFAULT_FONT = "tiny.ttf"
 
+local window = nil
+local renderer = nil
 
 local S = {
     color = {
         clear = { 0, 0, 0, 255 },
         draw  = { 255, 255, 255, 255 }
     },
-
     expert = 0,
-
     view = {
-        phy = CONFIG.Pico_Dim.new(500, 500)
+        phy = Pico_Dim(500, 500)
     },
-
     dim = {
         world = { y = 0 }
     },
-
     font = {
         ttf = nil,
         h   = 0
     },
-
     grid = 1,
-
     size = {
-        cur = CONFIG.Pico_Dim.new(100, 100),
-        org = CONFIG.Pico_Dim.new(100, 100)
+        cur = Pico_Dim(100, 100),
+        org = Pico_Dim(100, 100)
     }
 }
 
 function pico.input_delay(ms)
-    -- INCOMPLETO
     while true do
         local old = SDL.getTicks()
-        has = SDL.waitEvent(ms)
-
+        SDL.waitEvent(ms)
         local dt = SDL.getTicks() - old
         ms = ms - dt
-
-        if ms <= 0 then
-            return
-        end
+        if ms <= 0 then return end
     end
 end
 
 function pico.get_size()
-    return {phy = CONFIG.PHY(window), log = S.size.org}
+    return { phy = PHY(window), log = S.size.org }
 end
 
 function pico.set_font(file, h)
@@ -65,18 +82,13 @@ function pico.set_font(file, h)
             600
         h = math.max(8, math.floor(wy / 10))
     end
-
     S.font.h = h
-
     if S.font.ttf then
         S.font.ttf:close()
         S.font.ttf = nil
     end
-
-    local font_file = file or DEFAULT_FONT
-    local font, err = TTF.open(font_file, S.font.h)
+    local font, err = TTF.open(file or DEFAULT_FONT, S.font.h)
     assert(font, err)
-
     S.font.ttf = font
 end
 
@@ -87,9 +99,7 @@ function pico._output_clear()
         b = S.color.clear[3],
         a = S.color.clear[4]
     })
-
     renderer:clear()
-
     renderer:setDrawColor({
         r = S.color.draw[1],
         g = S.color.draw[2],
@@ -99,28 +109,19 @@ function pico._output_clear()
 end
 
 function pico.show_grid()
-    if not S.grid then
-        return
-    end
-
+    if not S.grid then return end
     renderer:setDrawColor(0x77, 0x77, 0x77, 0x77)
-
-    local phy = CONFIG.PHY(window)
-
+    local phy = PHY(window)
     renderer:setLogicalSize(phy.w, phy.h)
-
     local step_x = phy.w / S.size.cur.w
     for i = 0, phy.w, step_x do
         renderer:drawLine { i, 0, i, phy.h }
     end
-
     local step_y = phy.h / S.size.cur.h
     for j = 0, phy.h, step_y do
         renderer:drawLine { 0, j, phy.w, j }
     end
-
     renderer:setLogicalSize(S.size.cur.w, S.size.cur.h)
-
     renderer:setDrawColor({
         r = S.color.draw[1],
         g = S.color.draw[2],
@@ -130,28 +131,13 @@ function pico.show_grid()
 end
 
 function pico._output_present(force)
-
-    if S.expert and not force then
-        return
-    end
+    if S.expert and not force then return end
     renderer:setTarget(TEX)
-    renderer:setDrawColor({
-        r = 0x77,
-        g = 0x77,
-        b = 0x77,
-        a = 0x77
-    })
+    renderer:setDrawColor(0x77, 0x77, 0x77, 0x77)
     renderer:clear()
     renderer:copy(TEX)
     pico.show_grid()
     renderer:present()
-    renderer:setDrawColor({
-        r = S.color.draw[1],
-        g = S.color.draw[2],
-        b = S.color.draw[3],
-        a = S.color.draw[4]
-    })
-
     renderer:setTarget(TEX)
 end
 
@@ -165,24 +151,24 @@ function pico.output_clear()
 end
 
 function pico.set_grid(on)
-    S.grid = 0
+    S.grid = on and 1 or 0
     pico._output_present(0)
 end
 
 function pico._set_size(phy, log)
-    local PICO_SIZE_KEEP = CONFIG.PICO_SIZE_KEEP()
-    local PICO_SIZE_FULLSCREEN = CONFIG.PICO_SIZE_FULLSCREEN()
-    local PHY = CONFIG.PHY(window)
+    local KEEP = PICO_SIZE_KEEP()
+    local FULL = PICO_SIZE_FULLSCREEN()
+    local CUR  = PHY(window)
 
-    if phy.w ~= PICO_SIZE_KEEP.w or phy.h ~= PICO_SIZE_KEEP.h then
-        if phy.w == PICO_SIZE_FULLSCREEN.w and phy.h == PICO_SIZE_FULLSCREEN.h then
-            window:setSize(PHY.w, PHY.h)
+    if phy.w ~= KEEP.w or phy.h ~= KEEP.h then
+        if phy.w == FULL.w and phy.h == FULL.h then
+            window:setSize(CUR.w, CUR.h)
         else
             window:setSize(phy.w, phy.h)
         end
     end
 
-    if log.w ~= PICO_SIZE_KEEP.w or log.h ~= PICO_SIZE_KEEP.h then
+    if log.w ~= KEEP.w or log.h ~= KEEP.h then
         S.size.cur = log
         if TEX then renderer:destroyTexture(TEX) end
         TEX = renderer:createTexture(
@@ -194,7 +180,7 @@ function pico._set_size(phy, log)
         renderer:setLogicalSize(S.size.cur.w, S.size.cur.h)
     end
 
-    if PHY.w == S.size.cur.w or PHY.h == S.size.cur.h then
+    if CUR.w == S.size.cur.w or CUR.h == S.size.cur.h then
         S.grid = 0
     end
 
@@ -206,48 +192,40 @@ function pico.set_size(phy, log)
     pico._set_size(phy, log)
 end
 
-function pico.set_title(title)
-    window:setTitle(title)
+function pico.set_title(t)
+    title = t
+    if window then window:setTitle(t) end
 end
 
 function pico.init(on)
     if on then
         assert(SDL.init { SDL.flags.Video })
-
         window = assert(SDL.createWindow {
-            title  = CONFIG.title,
-            width  = CONFIG.undefined,
-            height = CONFIG.undefined,
+            title  = title,
+            width  = undefined,
+            height = undefined,
             x      = S.view.phy.w,
             y      = S.view.phy.h,
             flags  = { SDL.window.Shown }
         })
-
         renderer = assert(SDL.createRenderer(
             window, -1, SDL.rendererFlags.Accelerated
         ))
-
         renderer:setDrawBlendMode(SDL.blendMode.Blend)
-
         TTF.init()
         MIXER.openAudio(22050, SDL.audioFormat.S16, 2, 1024)
-
-        pico.set_size(CONFIG.PICO_DIM_PHY(), CONFIG.PICO_DIM_LOG())
+        pico.set_size(PICO_DIM_PHY(), PICO_DIM_LOG())
         pico.set_font(nil, 0)
         pico.output_clear()
-
     else
         if S.font.ttf then
             S.font.ttf:close()
             S.font.ttf = nil
         end
-
         MIXER.closeAudio()
         TTF.quit()
-
         if renderer then renderer:destroy() end
         if window then window:destroy() end
-
         SDL.quit()
     end
 end
