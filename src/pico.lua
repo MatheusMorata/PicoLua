3633b3d5caaffe424711bb97334701f541aac213
MatheusMorata <matlucar09@hotmail.com>
2026-01-26 14:28:59 -0300

    refactor: Lua idioms

--- src/pico.lua
+++ src/pico.lua
@@ -1,4 +1,8 @@
-local pico = {}
+local pico = {
+    set   = {},
+    get   = {},
+    input = {},
+}
 
 local SDL   = require "SDL"
 local TTF   = require "SDL.ttf"
@@ -8,6 +12,8 @@ local TEX = nil
 local title = "Titulo"
 local undefined = "0x1FFF0000"
 local DEFAULT_FONT = "tiny.ttf"
+
+local window, renderer
 
 local function Pico_Dim(w, h)
     return { w = w or 0, h = h or 0 }
@@ -35,7 +41,6 @@ local function PHY(window)
 end
 
 local S = {
-
     color = {
         clear = { 0, 0, 0, 255 },
         draw  = { 255, 255, 255, 255 }
@@ -57,84 +62,15 @@ local S = {
     },
 
     grid = 1,
+
     size = {
         cur = Pico_Dim(100, 100),
         org = Pico_Dim(100, 100)
     },
 
-    scroll = {
-        x = 0,
-        y = 0
-    },
-
-    zoom = {
-        x = 0,
-        y = 0
-    }
+    scroll = { x = 0, y = 0 },
+    zoom   = { x = 0, y = 0 }
 }
-
-function pico.set_grid(on) 
-    S.grid = on
-    pico._output_present(0)
-end
-
-function pico.set_scroll(pos) 
-    S.scroll = pos
-end
-
-function pico.set_zoom(zoom)
-    S.zoom = zoom
-
-    pico.set_scroll({
-        x = S.scroll.x - (S.size.org.x - S.size.cur.x) / 2,
-        y = S.scroll.y - (S.size.org.y - S.size.cur.y) / 2
-    })
-
-    pico._set_size(
-        PICO_SIZE_KEEP(),
-        {
-            w = S.size.org.w * 100 / zoom.x,
-            h = S.size.org.h * 100 / zoom.y
-        }
-    )
-
-    pico.set_scroll({
-        x = S.scroll.x + (S.size.org.x - S.size.cur.x) / 2,
-        y = S.scroll.y + (S.size.org.y - S.size.cur.y) / 2
-    })
-end
-
-function pico.input_delay(ms)
-    while true do
-        local old = SDL.getTicks()
-        SDL.waitEvent(ms)
-        local dt = SDL.getTicks() - old
-        ms = ms - dt
-        if ms <= 0 then return end
-    end
-end
-
-function pico.get_size()
-    return { phy = PHY(window), log = S.size.org }
-end
-
-function pico.set_font(file, h)
-    if not h or h == 0 then
-        local wy =
-            (S.dim.world  and S.dim.world.y)  or
-            (S.dim.window and S.dim.window.y) or
-            600
-        h = math.max(8, math.floor(wy / 10))
-    end
-    S.font.h = h
-    if S.font.ttf then
-        S.font.ttf:close()
-        S.font.ttf = nil
-    end
-    local font, err = TTF.open(file or DEFAULT_FONT, S.font.h)
-    assert(font, err)
-    S.font.ttf = font
-end
 
 function pico._output_clear()
     renderer:setDrawColor({
@@ -154,17 +90,22 @@ end
 
 function pico.show_grid()
     if not S.grid then return end
+
     renderer:setDrawColor(0x77, 0x77, 0x77, 0x77)
+
     local phy = PHY(window)
     renderer:setLogicalSize(phy.w, phy.h)
+
     local step_x = phy.w / S.size.cur.w
     for i = 0, phy.w, step_x do
         renderer:drawLine { i, 0, i, phy.h }
     end
+
     local step_y = phy.h / S.size.cur.h
     for j = 0, phy.h, step_y do
         renderer:drawLine { 0, j, phy.w, j }
     end
+
     renderer:setLogicalSize(S.size.cur.w, S.size.cur.h)
     renderer:setDrawColor({
         r = S.color.draw[1],
@@ -176,6 +117,7 @@ end
 
 function pico._output_present(force)
     if S.expert and not force then return end
+
     renderer:setTarget(TEX)
     renderer:setDrawColor(0x77, 0x77, 0x77, 0x77)
     renderer:clear()
@@ -183,20 +125,6 @@ function pico._output_present(force)
     pico.show_grid()
     renderer:present()
     renderer:setTarget(TEX)
-end
-
-function pico.output_present()
-    pico._output_present(1)
-end
-
-function pico.output_clear()
-    pico._output_clear()
-    pico._output_present(0)
-end
-
-function pico.set_grid(on)
-    S.grid = on and 1 or 0
-    pico._output_present(0)
 end
 
 function pico._set_size(phy, log)
@@ -214,13 +142,16 @@ function pico._set_size(phy, log)
 
     if log.w ~= KEEP.w or log.h ~= KEEP.h then
         S.size.cur = log
+
         if TEX then renderer:destroyTexture(TEX) end
+
         TEX = renderer:createTexture(
             SDL.pixelFormat.RGBA8888,
             SDL.textureAccess.Target,
             S.size.cur.w,
             S.size.cur.h
         )
+
         renderer:setLogicalSize(S.size.cur.w, S.size.cur.h)
     end
 
@@ -231,19 +162,98 @@ function pico._set_size(phy, log)
     pico._output_present(0)
 end
 
-function pico.set_size(phy, log)
+function pico.set.grid(on)
+    S.grid = on and 1 or 0
+    pico._output_present(0)
+end
+
+function pico.set.scroll(pos)
+    S.scroll = pos
+end
+
+function pico.set.zoom(zoom)
+    S.zoom = zoom
+
+    pico.set.scroll({
+        x = S.scroll.x - (S.size.org.x - S.size.cur.x) / 2,
+        y = S.scroll.y - (S.size.org.y - S.size.cur.y) / 2
+    })
+
+    pico._set_size(
+        PICO_SIZE_KEEP(),
+        {
+            w = S.size.org.w * 100 / zoom.x,
+            h = S.size.org.h * 100 / zoom.y
+        }
+    )
+
+    pico.set.scroll({
+        x = S.scroll.x + (S.size.org.x - S.size.cur.x) / 2,
+        y = S.scroll.y + (S.size.org.y - S.size.cur.y) / 2
+    })
+end
+
+function pico.set.font(file, h)
+    if not h or h == 0 then
+        local wy =
+            (S.dim.world  and S.dim.world.y)  or
+            (S.dim.window and S.dim.window.y) or
+            600
+        h = math.max(8, math.floor(wy / 10))
+    end
+
+    S.font.h = h
+
+    if S.font.ttf then
+        S.font.ttf:close()
+        S.font.ttf = nil
+    end
+
+    local font, err = TTF.open(file or DEFAULT_FONT, S.font.h)
+    assert(font, err)
+    S.font.ttf = font
+end
+
+function pico.set.size(phy, log)
     S.size.org = log
     pico._set_size(phy, log)
 end
 
-function pico.set_title(t)
+function pico.set.title(t)
     title = t
     if window then window:setTitle(t) end
+end
+
+function pico.get.size()
+    return {
+        phy = PHY(window),
+        log = S.size.org
+    }
+end
+
+function pico.input.delay(ms)
+    while true do
+        local old = SDL.getTicks()
+        SDL.waitEvent(ms)
+        local dt = SDL.getTicks() - old
+        ms = ms - dt
+        if ms <= 0 then return end
+    end
+end
+
+function pico.output_clear()
+    pico._output_clear()
+    pico._output_present(0)
+end
+
+function pico.output_present()
+    pico._output_present(1)
 end
 
 function pico.init(on)
     if on then
         assert(SDL.init { SDL.flags.Video })
+
         window = assert(SDL.createWindow {
             title  = title,
             width  = undefined,
@@ -252,24 +262,31 @@ function pico.init(on)
             y      = S.view.phy.h,
             flags  = { SDL.window.Shown }
         })
+
         renderer = assert(SDL.createRenderer(
             window, -1, SDL.rendererFlags.Accelerated
         ))
+
         renderer:setDrawBlendMode(SDL.blendMode.Blend)
+
         TTF.init()
         MIXER.openAudio(22050, SDL.audioFormat.S16, 2, 1024)
-        pico.set_size(PICO_DIM_PHY(), PICO_DIM_LOG())
-        pico.set_font(nil, 0)
+
+        pico.set.size(PICO_DIM_PHY(), PICO_DIM_LOG())
+        pico.set.font(nil, 0)
         pico.output_clear()
     else
         if S.font.ttf then
             S.font.ttf:close()
             S.font.ttf = nil
         end
+
         MIXER.closeAudio()
         TTF.quit()
+
         if renderer then renderer:destroy() end
         if window then window:destroy() end
+
         SDL.quit()
     end
 end
--- tst/main.lua
+++ tst/main.lua
@@ -3,5 +3,5 @@ local titulo = "Novo"
 
 
 pico.init(true) -- Inicializa
-pico.set_title(titulo) -- Define o título da janela
-pico.input_delay(2000) -- Mantém a janela aberta no tempo de 2000 milisegundos
+pico.set.title(titulo) -- Define o título da janela
+pico.input.delay(2000) -- Mantém a janela aberta no tempo de 2000 milisegundos
