24836ba1114c69b5d16d3486c76ca095484f8259
MatheusMorata <matlucar09@hotmail.com>
2026-03-16 20:00:53 -0300

    tst: exemplo.lua

--- tst/exemplo.lua
+++ tst/exemplo.lua
@@ -1,15 +1,23 @@
 local pico = dofile("../src/pico.lua")
+
+-- inicia SDL
 pico.init(true)
+
+-- define tamanho da janela
 pico.set.size(
-    { w = 160, h = 160 }, 
-    { w = 16,  h = 16 }  
+    {w = 160, h = 160},   -- tamanho físico
+    {w = 16,  h = 16}     -- tamanho lógico
 )
 
+-- desenha o X animado
 for i = 0, 15 do
-    pico.output.draw_pixel({ x = i,     y = i })
-    pico.output.draw_pixel({ x = 15-i,  y = i })
+    pico.output.draw_pixel({x = i, y = i})
+    pico.output.draw_pixel({x = 15 - i, y = i})
     pico.input.delay(100)
 end
 
+-- espera 1 segundo
 pico.input.delay(1000)
+
+-- finaliza SDL
 pico.init(false)
