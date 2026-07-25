8bcde5c4d77a04a4f1126c9f00b7ac1efbd6a619
MatheusMorata <matlucar09@hotmail.com>
2026-07-25 18:31:51 -0300

    test: experimento2.lua

--- tst/experimento2.lua
+++ tst/experimento2.lua
@@ -2,6 +2,11 @@ local pico = dofile("../src/pico.lua")
 
 pico.init(true)
 
+pico.set.size(
+    { x = 800, y = 600 },
+    { x = 80,  y = 60 }
+)
+
 pico.set.color_draw({
     r = 0,
     g = 255,
@@ -9,11 +14,38 @@ pico.set.color_draw({
     a = 255
 })
 
+pico.set.anchor_draw({
+    x = PICO_LEFT,
+    y = PICO_TOP
+})
+
+local canto = pico.pos({
+    x = 0,
+    y = 0
+})
+
 pico.output.draw_rect({
-    x = 27,
-    y = 19,
+    x = canto.x,
+    y = canto.y,
     w = 10,
-    h = 10
+    h = 5
+})
+
+pico.set.anchor_draw({
+    x = PICO_CENTER,
+    y = PICO_MIDDLE
+})
+
+local centro = pico.pos({
+    x = 50,
+    y = 50
+})
+
+pico.output.draw_rect({
+    x = centro.x,
+    y = centro.y,
+    w = 4,
+    h = 4
 })
 
 while true do
