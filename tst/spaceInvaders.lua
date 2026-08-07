81410003a42fd9148947ef9360461a10ea9ccf3b
MatheusMorata <matlucar09@hotmail.com>
2026-08-07 12:24:41 -0300

    tst: Iniciando desenho nave

--- tst/spaceInvaders.lua
+++ tst/spaceInvaders.lua
@@ -1,7 +1,9 @@
 local pico = dofile("../src/pico.lua")
+local utils = dofile("utils.lua")
 
 pico.init(true)
 
+utils.nave(pico)   -- desenha o quadrado verde (nave)
 
 -- loop basico pra manter a janela viva
 while true do
--- tst/utils.lua
+++ tst/utils.lua
@@ -1,1 +1,15 @@
-local pico = dofile("../src/pico.lua")
+local utils = {}
+
+function utils.nave(pico)
+    pico.set.color_draw({ r = 0, g = 255, b = 0, a = 255 })
+    pico.set.style('PICO_FILL')
+
+    pico.output.draw_rect({
+        x = 10,  -- posição horizontal
+        y = 10,  -- posição vertical
+        w = 6,   -- largura pequena
+        h = 6,   -- altura pequena
+    })
+end
+
+return utils
